98cb8bae520d159bddc9bc533b666a94cb61e03c
lulch <lulchevagergana@gmail.com>
2021-02-02 16:56:42 +0200

    defining classes exercises

---
+++ src/DefiningClassesExercises/Google/Parents.java
@@ -0,0 +1,16 @@
+package DefiningClassesExercises.Google;
+
+public class Parents {
+    private String name;
+    private String birthday;
+
+    public Parents(String name, String birthday){
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s\n",name,birthday);
+    }
+}
